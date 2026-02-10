5b5bd7d1ddc4f1e9f8e830c718db8b85777edde6
adelsonsljunior <junioradelsonst@gmail.com>
2026-02-10 20:53:59 -0300

    style: format string literals and improve code consistency

--- services/writer/main.py
+++ services/writer/main.py
@@ -3,12 +3,12 @@ import socketserver
 import json
 import redis
 
-class RequestHandler(http.server.SimpleHTTPRequestHandler):
 
+class RequestHandler(http.server.SimpleHTTPRequestHandler):
     def _send_cors_headers(self):
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Methods', '*')
-        self.send_header('Access-Control-Allow-Headers', '*')
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Methods", "*")
+        self.send_header("Access-Control-Allow-Headers", "*")
 
     def do_OPTIONS(self):
         self.send_response(200, "ok")
@@ -21,22 +21,22 @@ class RequestHandler(http.server.SimpleHTTPRequestHandler):
         self._send_cors_headers()
         self.end_headers()
 
-        if self.path == '/health':
+        if self.path == "/health":
             self.wfile.write(bytes(f"up", "utf8"))
 
         return
+
     def do_POST(self):
         self.send_response(201)
         self.send_header("Content-type", "application/json")
         self._send_cors_headers()
         self.end_headers()
-        if self.path == '/write':
-            content_length = int(self.headers['Content-Length'])
+        if self.path == "/write":
+            content_length = int(self.headers["Content-Length"])
             post_data = self.rfile.read(content_length)
             redishost = "redis"
             redisclient = redis.Redis(host=redishost)
-            redisclient.set("SHAREDKEY",post_data.decode('utf-8'))
-
+            redisclient.set("SHAREDKEY", post_data.decode("utf-8"))
 
 
 handler_object = RequestHandler
