d3504f7f98a6bb41646c0365f64c6b25db366bf7
adelsonsljunior <junioradelsonst@gmail.com>
2026-02-10 22:14:44 -0300

    refactor(reader): clean up formatting and maintain consistent indentation in main.go

--- services/reader/main.go
+++ services/reader/main.go
@@ -1,41 +1,39 @@
 package main
 
 import (
-    "fmt"
-    "github.com/go-redis/redis"
-    "github.com/rs/cors"
-    "log"
-    "net/http"
+	"fmt"
+	"github.com/go-redis/redis"
+	"github.com/rs/cors"
+	"log"
+	"net/http"
 )
 
-
 func main() {
-    redis_host := "redis"
-    redis_port := "6379"
-    mux := http.NewServeMux()
+	redis_host := "redis"
+	redis_port := "6379"
+	mux := http.NewServeMux()
 
-    client := redis.NewClient(&redis.Options{Addr: redis_host+":"+redis_port})
-    key := client.Get("SHAREDKEY")
+	client := redis.NewClient(&redis.Options{Addr: redis_host + ":" + redis_port})
+	key := client.Get("SHAREDKEY")
 
-    mux.HandleFunc("/health", func(writer http.ResponseWriter, request *http.Request){
-        if request.Method == "OPTIONS" {
-            writer.WriteHeader(http.StatusOK)
-            return
-        }
-        fmt.Fprintf(writer, "up")
-    })
-    
+	mux.HandleFunc("/health", func(writer http.ResponseWriter, request *http.Request) {
+		if request.Method == "OPTIONS" {
+			writer.WriteHeader(http.StatusOK)
+			return
+		}
+		fmt.Fprintf(writer, "up")
+	})
 
-    mux.HandleFunc("/data", func(writer http.ResponseWriter, request *http.Request) {
-        fmt.Fprintf(writer, key.Val())
-    })
+	mux.HandleFunc("/data", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprintf(writer, key.Val())
+	})
 
-    handler := cors.New(cors.Options{
-        AllowedOrigins: []string{"*"},
-        AllowedHeaders: []string{"*"},
-    }).Handler(mux)
-    
-    log.Println("Server starting on :8081")
-    log.Fatal(http.ListenAndServe(":8081", handler))
+	handler := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowedHeaders: []string{"*"},
+	}).Handler(mux)
+
+	log.Println("Server starting on :8081")
+	log.Fatal(http.ListenAndServe(":8081", handler))
 
 }
